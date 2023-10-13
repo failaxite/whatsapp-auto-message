47108925bc8633a1372c9d4c736d5cb1b94e8241
failaxite <failaxite.don@gmail.com>
2023-10-13 20:31:41 +0200

    :bug: Fixing button for sending message

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")
 
 os.system("")
 os.environ["WDM_LOG_LEVEL"] = "0"
+
 class style():
     BLACK = '\033[30m'
     RED = '\033[31m'
@@ -62,17 +63,15 @@ for idx, number in enumerate(numbers):
 			if not sent:
 				driver.get(url)
 				try:
-					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='compose-btn-send']")))
+					send_button = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//span[@data-icon='send']")))
+                    send_button.click()
+                    sent = True
+                    sleep(5)
+                    print(style.GREEN + {translations.get('message_sent')}+ style.RESET)
 				except Exception as e:
 					print(style.RED + f"\nFailed to send message to: {number}, retry ({i+1}/3)")
 					print("Make sure your phone and computer is connected to the internet.")
 					print("If there is an alert, please dismiss it." + style.RESET)
-				else:
-					sleep(1)
-					click_btn.click()
-					sent=True
-					sleep(3)
-					print(style.GREEN + 'Message sent to: ' + number + style.RESET)
 	except Exception as e:
 		print(style.RED + 'Failed to send message to ' + number + str(e) + style.RESET)
 driver.close()
